e660979329e0806c4fc32f50a51e925137ec7b4c
yeongjun Kim <kyjun519@gmail.com>
2023-05-25 07:08:00 +0900

    fix: Image 버그 수정

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import "swiper/swiper.min.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Pagination, Autoplay } from "swiper";
@@ -23,9 +22,9 @@ const Slider = ({ images }: SliderProp) => {
         scrollbar={{ draggable: true }}
         navigation
       >
-        {images.map((image) => (
-          <SwiperSlide>
-            <img src={image} className="w-full h-64" alt="제품 이미지" />
+        {images.map((image, index) => (
+          <SwiperSlide key={index}>
+            <Image src={image} className="w-full h-64" width={500} height={1} alt="제품 이미지" />
           </SwiperSlide>
         ))}
         <span className="pagination_fraction  absolute bottom-3 right-3 z-10 bg-blue-500 text-white text-base px-4 font-medium rounded-full"></span>
