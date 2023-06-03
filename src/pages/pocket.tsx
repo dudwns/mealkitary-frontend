d8f5c29ae3be28be7df0b66ed3c75f36e1e61dc3
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2023-06-04 00:01:57 +0900

    layout: header -> Nav, layout update

--- src/pages/pocket.tsx
+++ src/pages/pocket.tsx
@@ -1,4 +1,3 @@
-import Header from "@/components/header";
 import Layout from "@/components/layout";
 import Nav from "@/components/nav";
 import TabBar from "@/components/tabBar";
@@ -6,44 +5,31 @@ import { reservationMenuName, totalPriceState } from "@/lib/recoilState";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useRecoilState } from "recoil";
-import { MenuItem } from "./order";
+
 import Shop from "./shop/[id]";
 
-interface shop {
-  id: number;
-  title: string;
-  images: string[] | null;
-  description: string;
-  menus: MenuItem[];
-}
-
-export default function Pocket({
-  id,
-  name,
-  image,
-  price,
-  description,
-}: MenuItem) {
+export default function Pocket() {
   const router = useRouter();
 
   const [menuName, setMenuName] = useRecoilState(reservationMenuName);
   const [totalPrice, setTotalPrice] = useRecoilState(totalPriceState);
   return (
     <Layout>
-      <Nav>
-        <Header title="장바구니" />
+      <Nav backBtn={true}>
+        <div>장바구니</div>
       </Nav>
       <div className="flex flex-col mt-16 border-t-2 shadow-md px-6 bg-white">
-        <h3>예약한 메뉴</h3>
-        <ul>
-          {[1, 1].map((data, index) => (
-            <li key={index}>
-              <div>{name}</div>
-              <div>{image}</div>
-              <div>{price}</div>
-            </li>
-          ))}
-        </ul>
+        <h3 className="font-bold text-lg mt-3">예약한 메뉴</h3>
+        {[1, 1, 1].map((i) => (
+          <div key={i} className="flex border-b">
+            <div className="w-12 h-12 bg-gray-400 rounded-md mr-2 my-5 " />
+            <div className="flex flex-col py-4">
+              <span className="text-sm">메뉴 이름</span>
+              <span className="text-xs">설명</span>
+              <span className="text-xs">가격 : 13000</span>
+            </div>
+          </div>
+        ))}
         <h3
           className="text-center cursor-pointer py-4"
           onClick={() => router.back()}
@@ -58,10 +44,14 @@ export default function Pocket({
         </div>
         <div className="flex justify-between mx-2 py-2 border-b">
           <span>결제 수단</span>
-          <span className="text-blue-700">⬇</span>
+          <span className="text-blue-700 cursor-pointer">⬇</span>
+        </div>
+        <div className="flex justify-between mx-2 py-2 border-b">
+          <span>예약 하기</span>
+          <span className="text-blue-700 cursor-pointer">⬇</span>
         </div>
       </div>
-      <TabBar text="예약하기" onClick={() => router.push("/reservationDate")} />
+      <TabBar text="결제하기" onClick={() => router.push("/payment")} />
     </Layout>
   );
 }
