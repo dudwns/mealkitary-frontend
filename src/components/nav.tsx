5372b885ac6fd4abc697fcfe31d10012f3050291
yeongjun Kim <kyjun519@gmail.com>
2023-06-04 18:36:08 +0900

    feat: Back 버튼 동작 구현

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,15 +1,19 @@
+import { useRouter } from "next/router";
+
 interface NavProps {
   backBtn?: boolean;
+  title?: string;
   children: React.ReactNode;
 }
 
-export default function Nav({ backBtn, children }: NavProps) {
+export default function Nav({ backBtn, title, children }: NavProps) {
+  const router = useRouter();
   return (
     <>
       <div className="bg-white w-full h-14 max-w-xl text-lg font-medium  fixed left-0 right-0 mx-auto top-0 text-gray-800 flex justify-center items-center z-20 border-b-2 border-gray-300 shadow-md">
         {children}
         {backBtn ? (
-          <button className="absolute left-4">
+          <button className="absolute left-4" onClick={() => router.back()}>
             <svg
               className="w-6 h-6 text-blue-500"
               fill="none"
