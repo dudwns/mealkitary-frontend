7a1cd0c553a73afb0df1eefedf341169c099655a
yeongjun Kim <kyjun519@gmail.com>
2023-06-04 18:43:44 +0900

    feat: 파일 삭제

--- src/components/reservationTime.tsx
+++
@@ -1,9 +0,0 @@
-interface times {
-  time: string;
-}
-
-export default function RerservationTime({ time }: times) {
-  return (
-    <span className="bg-slate-400 rounded-md p-2 text-center">{time}</span>
-  );
-}
