5b39857911fca2c35b9eb1b3186f938f98ab682c
yeongjun Kim <kyjun519@gmail.com>
2023-08-22 16:26:03 +0900

    feat: Spinner 컴포넌트 구현

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,44 @@
+interface SpinnerProp {
+  size: number;
+  color: string;
+  loading: boolean;
+}
+
+const Spinner = ({ size = 24, color = "#919EAB", loading = true, ...props }: SpinnerProp) => {
+  const sizeStyle = {
+    width: size,
+    height: size,
+  };
+  return loading ? (
+    <div className="inline-block align-middle">
+      <svg viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg" style={sizeStyle}>
+        <g fill="none" fillRule="evenodd">
+          <g transform="translate(1 1)">
+            <path d="M36 18c0-9.94-8.06-18-18-18" stroke={color} strokeWidth="2">
+              <animateTransform
+                attributeName="transform"
+                type="rotate"
+                from="0 18 18"
+                to="360 18 18"
+                dur="0.9s"
+                repeatCount="indefinite"
+              />
+            </path>
+            <circle fill={color} cx="36" cy="18" r="1">
+              <animateTransform
+                attributeName="transform"
+                type="rotate"
+                from="0 18 18"
+                to="360 18 18"
+                dur="0.9s"
+                repeatCount="indefinite"
+              />
+            </circle>
+          </g>
+        </g>
+      </svg>
+    </div>
+  ) : null;
+};
+
+export default Spinner;
---
+++ src/stories/Spinner.stories.js
@@ -0,0 +1,19 @@
+import Spinner from "../components/Spinner";
+
+export default {
+  title: "Component/Spinner",
+  component: Spinner,
+  args: {
+    size: 80,
+    loading: true,
+  },
+  argTypes: {
+    size: { control: "number" },
+    color: { control: "color" },
+    loading: { control: "boolean" },
+  },
+};
+
+export const Default = (args) => {
+  return <Spinner {...args} />;
+};
