292283caa736cd7da4767f4969034da3b9cce7b9
Yeongjun Kim <39931980+dudwns@users.noreply.github.com>
2023-09-22 03:01:58 +0900

    feat: #12 - fail message 예외 처리 추가

--- src/pages/fail.tsx
+++ src/pages/fail.tsx
@@ -9,7 +9,7 @@ export default function FailPage() {
   return (
     <main style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <h1>결제 실패</h1>
-      <p>이유: {query.message ?? message.title}</p>
+      <p>이유: {query.message ?? message?.title}</p>
     </main>
   );
 }
